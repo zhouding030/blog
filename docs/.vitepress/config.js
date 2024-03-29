7c30e3aa9fd7b7c9007b1309fb7b0b3f74b21bc2
zhouding <zhouding@gingo.cn>
2024-03-30 00:38:57 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,5 @@
 export default {
-    title: '个人博客',
+    title: 'ZhouDing个人博客',
     description: '我的第一个博客',
     // 打包目录
     dest: './dist',
@@ -19,7 +19,8 @@ export default {
                         link: "/aboutme/0.md",
                     }
                 ],
-            }, {
+            },
+            {
                 text: "DDD领域驱动设计",
                 collapsible: true,
                 items: [
@@ -29,9 +30,127 @@ export default {
                     },{
                         text: "战术设计",
                         link: "/ddd/1.md",
-                    }
+                    },{
+                         text: "领域建模",
+                         link: "/ddd/2.md",
+                     },{
+                         text: "落地实战",
+                         link: "/ddd/3.md",
+                     }
                 ],
-            }
+            },
+            {
+                 text: "架构师知识体系",
+                 collapsible: true,
+                 items: [
+                     {
+                         text: "Java",
+                         items: [
+                            {
+                                 text: "java基础知识",
+                                 items: [
+                                    {
+                                        text: "java数据类型",
+                                        link: "/java/java基础知识/java数据类型.md",
+                                    },
+                                     {
+                                          text: "Object对象",
+                                          link: "/java/java基础知识/Object对象.md",
+                                     },
+                                     {
+                                          text: "其他...",
+                                          link: "/java/java基础知识/其他.md",
+                                     }
+                                 ]
+                            },{
+                                text: "并发编程与多线程",
+                                link: "/java/并发编程与多线程/0.md"
+                            },{
+                                 text: "集合原理",
+                                 link: "/java/集合原理/0.md"
+                            },{
+                                 text: "JVM原理",
+                                 link: "/java/JVM原理/0.md"
+                            },{
+                                 text: "IO与网络编程",
+                                 link: "/java/IO与网络编程/0.md"
+                            },{
+                                 text: "设计模式",
+                                 link: "/java/设计模式/0.md"
+                            }
+                         ]
+                     },{
+                         text: "Spring 全家桶",
+                         items: [
+                            {
+                                text: "Spring 框架",
+                                link: "/spring全家桶/spring框架/0.md"
+                            },{
+                                 text: "Spring MVC 框架",
+                                 link: "/spring全家桶/springMVC框架/0.md"
+                            },{
+                                 text: "Spring Boot 框架",
+                                 link: "/spring全家桶/springboot框架/0.md"
+                            },{
+                                 text: "Spring Cloud 框架",
+                                 link: "/spring全家桶/springcloud框架/0.md"
+                            }
+                         ]
+                    },{
+                        text: "框架、中间件、技术栈",
+                        items: [
+                            {
+                                text: "Dubbo 框架",
+                                link: "/框架、中间件、技术栈/dubbo/0.md"
+                            },
+                            {
+                                text: "Netty 框架",
+                                link: "/框架、中间件、技术栈/netty/0.md"
+                            },
+                            {
+                                text: "MyBatis 框架",
+                                link: "/框架、中间件、技术栈/mybatis/0.md"
+                            },
+                            {
+                                text: "MySQL",
+                                link: "/框架、中间件、技术栈/mysql/mysql工作原理.md"
+                            },
+                            {
+                                text: "Redis",
+                                link: "/框架、中间件、技术栈/redis/0.md"
+                            },
+                            {
+                                text: "Kafka",
+                                link: "/框架、中间件、技术栈/kafka/0.md"
+                            },
+                            {
+                                text: "ElasticSearch",
+                                link: "/框架、中间件、技术栈/es/0.md"
+                            }
+                        ]
+                    }
+                 ]
+            },
+             {
+                  text: "数据结构与算法",
+                  collapsible: true,
+                  items: [
+                      {
+                          text: "排序算法",
+                          link: "/algorithm/0.md",
+                      }
+                  ],
+             },
+               {
+                    text: "项目管理经验",
+                    collapsible: true,
+                    items: [
+                        {
+                            text: "项目管理经验浅谈",
+                            link: "/项目管理经验/0.md",
+                        }
+                    ],
+               }
         ]
     }
 }
--- docs/index.md
+++ docs/index.md
@@ -2,10 +2,10 @@
 layout: home
 hero:
     image:
-      alt: 个人博客
-    name: 个人博客
-    text: 关注技术
-    tagline: 整合个人技术资源
+      alt: ZhouDing个人博客
+    name: 架构师的探索之旅
+    text: 关注技术、分享技术
+    tagline: 欢迎来到“架构师的探索之旅”，这里是我分享架构技术、实践经验、思考感悟的乐园。作为一名资深的架构师，我深知在这个快速变化的技术世界里，不断学习和探索的重要性。因此，我创建了这个博客，希望能与广大技术爱好者一起交流、成长。
     actions:
       - theme: brand
         text: 开始阅读
@@ -16,20 +16,12 @@ hero:
 
 
 features:
-    - title: 与开发语言无关
-      details: 基于Java语言讲解，不涉及高级语法，不与小众框架绑定
-    - title: 充血模型
-      details: 基于充血模型完整实践DDD
-    - title: 代码级讲解
-      details: 理论联系实际，概念不再枯燥
-    - title: 知识体系化
-      details: 融入TDD、生态、规范等知识，形成完整的体系
-    - title: 与开发语言无关
-      details: 基于Java语言讲解，不涉及高级语法，不与小众框架绑定
-    - title: 充血模型
-      details: 基于充血模型完整实践DDD
-    - title: 代码级讲解
-      details: 理论联系实际，概念不再枯燥
-    - title: 知识体系化
-      details: 融入TDD、生态、规范等知识，形成完整的体系
+    - title: DDD领域驱动设计
+      details: 基于DDD领域驱动设计进行落地实战
+    - title: 架构师知识体系
+      details: 架构师知识体系构建
+    - title: 数据结构与算法
+      details: 各类数据结构算法实现方案分析
+    - title: 项目管理经验
+      details: 针对项目管理中，各方面经验分享
 ---
